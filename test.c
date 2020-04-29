e11eba9a35264ceed4dcac22238e09ffdbecdbd7
William Casarin <jb55@jb55.com>
2020-04-29 12:42:27 -0700

    test: don't test invalid link data
    Signed-off-by: William Casarin <jb55@jb55.com>

--- test.c
+++ test.c
@@ -44,11 +44,19 @@ static struct test_case test_cases[] = {
 	  .expected_valid = true,
 	},
 
+	{ .url = "zoommtg://derpy.zoom.us/start?confid=12345125&pwd=hi",
+	  .expected_meeting_id = "",
+	  .expected_password = "hi",
+	  .expected_hostname = "derpy.zoom.us",
+	  .expected_mtg = "",
+	  .expected_valid = false,
+	},
+
 	{ .url = "h",
 	  .expected_meeting_id = "",
 	  .expected_password = "",
 	  .expected_hostname = "h",
-	  .expected_mtg = "zoommtg://h/start?confno=",
+	  .expected_mtg = "",
 	  .expected_valid = false,
 	},
 
@@ -56,7 +64,7 @@ static struct test_case test_cases[] = {
 	  .expected_meeting_id = "",
 	  .expected_password = "",
 	  .expected_hostname = "",
-	  .expected_mtg = "zoommtg:///start?confno=",
+	  .expected_mtg = "",
 	  .expected_valid = false,
 	},
 };
@@ -72,34 +80,35 @@ static int test_parse()
 		init_zoom_link(&link);
 		parse_zoom_link(test_case->url, &link);
 
-		if (!streq(link.meeting_id, test_case->expected_meeting_id)) {
-			printf("FAIL expected meeting_id '%s', got '%s'\n",
-			       test_case->expected_meeting_id, link.meeting_id);
-		}
-
-		if (!streq(link.password, test_case->expected_password)) {
-			printf("FAIL expected password '%s', got '%s'\n",
-			       test_case->expected_password, link.password);
-		}
-
-		if (!streq(link.hostname, test_case->expected_hostname)) {
-			printf("FAIL expected hostname '%s', got '%s'\n",
-			       test_case->expected_hostname, link.hostname);
-		}
-
-		make_zoommtg(&link, buffer, sizeof(buffer));
-
-		if (!streq(buffer, test_case->expected_mtg)) {
-			printf("FAIL expected mtg '%s', got '%s'\n",
-			       test_case->expected_mtg, buffer);
-		}
-
 		bool valid = validate_zoom_link(&link);
 		if (valid != test_case->expected_valid) {
 			printf("FAIL expected %s zoom link for '%s', got %s\n",
 			       test_case->expected_valid?"valid":"invalid",
 			       test_case->url,
 			       test_case->expected_valid?"invalid":"valid");
+		}
+
+		if (valid) {
+			if (!streq(link.meeting_id, test_case->expected_meeting_id)) {
+				printf("FAIL expected meeting_id '%s', got '%s'\n",
+				       test_case->expected_meeting_id, link.meeting_id);
+			}
+
+			if (!streq(link.password, test_case->expected_password)) {
+				printf("FAIL expected password '%s', got '%s'\n",
+				       test_case->expected_password, link.password);
+			}
+
+			if (!streq(link.hostname, test_case->expected_hostname)) {
+				printf("FAIL expected hostname '%s', got '%s'\n",
+				       test_case->expected_hostname, link.hostname);
+			}
+
+			make_zoommtg(&link, buffer, sizeof(buffer));
+			if (!streq(buffer, test_case->expected_mtg)) {
+				printf("FAIL expected mtg '%s', got '%s'\n",
+				       test_case->expected_mtg, buffer);
+			}
 		}
 
 		free_zoom_link(&link);
